90ab52b1b612257ec8409546b38acd1cdb50cf4e
unknown <bryantdalton19@gmail.com>
2023-06-19 11:13:28 -0400

    soundbtn css cleanup

--- src/pages/components/soundButton.jsx
+++ src/pages/components/soundButton.jsx
@@ -10,39 +10,30 @@ const StyledButton = styled.button`
   box-shadow: inset 0 0 0.5em 0 var(--grn-neon), 0 0 0.5em 0 var(--grn-neon);
   font-size: 2rem;
   cursor: pointer;
-  text-decoration: none;
   padding: 0.25em 1em;
   border-radius: 0.25em;
-  position: relative;
   transition: background-color 100ms linear;
   outline: 0;
   &:hover {
     background: var(--grn-neon);
     color: hsl(323 21% 16%);
-    text-shadow: none;
   }
   .theme-red & {
     color: var(--red-neon);
-    background-color: hsl(323 21% 16%);
     border: var(--red-neon) 0.125em solid;
-    text-shadow: 0 0 0.125em hsl(0 0% 100% / 0.5), 0 0 0.5em currentcolor;
     box-shadow: inset 0 0 0.5em 0 var(--red-neon), 0 0 0.5em 0 var(--red-neon);
     &:hover {
       background: var(--red-neon);
       color: hsl(323 21% 16%);
-      text-shadow: none;
     }
   }
   .theme-blue & {
     color: var(--blue-neon);
-    background-color: hsl(323 21% 16%);
     border: var(--blue-neon) 0.125em solid;
-    text-shadow: 0 0 0.125em hsl(0 0% 100% / 0.5), 0 0 0.5em currentcolor;
     box-shadow: inset 0 0 0.5em 0 var(--blue-neon), 0 0 0.5em 0 var(--blue-neon);
     &:hover {
       background: var(--blue-neon);
       color: hsl(323 21% 16%);
-      text-shadow: none;
     }
   }
 `;
